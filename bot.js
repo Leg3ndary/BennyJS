c4cce227ea01e35b5c740dfed1d74b230a8918ef
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2022-01-10 16:02:21 -0500

    Update bot file

--- bot.js
+++ bot.js
@@ -1,41 +1,40 @@
 // Require the necessary discord.js classes
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const discord = require('discord.js');
+const { BotToken } = require('./config/config.json');
 const fs = require('fs');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const bot = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+bot.once('ready', () => {
+	console.log(`${bot.user.username} Ready`);
 });
 
 
-client.on('interactionCreate', async interaction => {
+bot.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+	const slashCommand = bot.slashCommands.get(interaction.commandName);
 
-	if (!command) return;
+	if (!slashCommand) return;
 
 	try {
-		await command.execute(interaction);
+		await slashCommand.execute(interaction);
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+bot.slashCommands = new discord.Collection();
+const commandFiles = fs.readdirSync('./slashCommands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+	const slashCommand = require(`./slashCommands/${file}`);
+	bot.slashCommands.set(slashCommand.data.name, slashCommand);
 }
 
-// Login to Discord with your client's token
-client.login(token);
+
+
+bot.login(BotToken);
